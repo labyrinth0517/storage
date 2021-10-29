ff78615db0ea532ee86ffdfde53c758b58c42d0e
labyrinth0517 <73677685+labyrinth0517@users.noreply.github.com>
2021-10-29 14:06:20 +0800

    Add files via upload

--- day9_1/SaleCalculation.py
+++ day9_1/SaleCalculation.py
@@ -2,13 +2,15 @@ import xlrd
 from tool1 import *
 
 wb = xlrd.open_workbook(filename='excelD7.xls')
+param1 = []
 
-for i in range(12):
+for i in range(1):
     ts = wb.sheet_by_index(i)
-    for j in range(1, ts.nrows):
+    for j in range(1, 10):
         gcv = ts.row_values(j)
+        for x in gcv:
+            param1.append(x)
         sql1 = 'insert into saleDat value(%s,%s,%s,%s,%s)'
-        param1 = [gcv[0], gcv[1], gcv[2], gcv[3], gcv[4]]
         update(sql1, param1)
-
+        param1 = []
 print('end.')
